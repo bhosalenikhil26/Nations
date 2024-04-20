5029d31dfe1f516419bbeb8bb99b2637d752a939
Nikhil Bhosale <nikhil.bhosale@voiapp.io>
2024-04-20 10:34:52 +0200

    Created new project

---
+++ Nations/Nations/NationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationsApp.swift
+//  Nations
+//
+//  Created by Nikhil Bhosale on 2024-04-20.
+//
+
+import SwiftUI
+
+@main
+struct NationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
